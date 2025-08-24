5aec4876be88ec45ff662ac6f7c0f13a6a1eba4b
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrelltakudzwa9@gmail.com>
2025-08-24 09:27:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ app.post('/obfuscate', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 7860;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
